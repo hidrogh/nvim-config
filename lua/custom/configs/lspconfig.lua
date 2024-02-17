8d967cd0a1e07feb2bce980b0f7cdefb606553ff
hidrogh <itshidropm@proton.me>
2024-02-17 21:15:25 +0100

    nvim-config

---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,47 @@
+local base = require("plugins.configs.lspconfig")
+local on_attach = base.on_attach
+local capabilities = base.capabilities
+
+local lspconfig = require("lspconfig")
+local util = require "lspconfig/util"
+lspconfig.clangd.setup {
+  on_attach = function(client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
+  capabilities = capabilities,
+}
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"rust"},
+  root_dir = util.root_pattern("Cargo.toml"),
+  settings = {
+    ['rust_analyzer'] = {
+      cargo = {
+        allFeatures = true,
+      },
+    },
+  },
+})
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"python"},
+})
+lspconfig.gopls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {"gopls"},
+  filetypes = {"go", "gomod", "gowork", "gotmpl"},
+  root_dir = util.root_pattern("go.work", "god.mod", ".git")
+})
+lspconfig.tsserver.setup({
+  on_attach = on_attach,
+  capabilities=capabilities,
+  init_options = {
+    preferences = {
+      disableSuggestions = true,
+    }
+  }
+})
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,38 @@
+local plugins = {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",
+        "rust-analyzer",
+        "clang-format",
+        "pyright",
+        "black",
+        "gopls",
+        "typescript-language-server",
+      }
+    }
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end
+  }
+}
+return plugins
